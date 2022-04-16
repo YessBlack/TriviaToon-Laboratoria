58cb32f2a025cc0632826352605af9fee1d6380f
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2022-04-16 08:56:07 -0500

    version 1

---
+++ js/app.js
@@ -0,0 +1,122 @@
+//modal nick jugador
+const play = document.getElementById('play')
+
+play.addEventListener('click', e => {
+    const player = document.getElementById('player').value
+
+    //pinta en el DOM el nombre del jugador
+    const playerDOM = document.querySelector('.player-dom')
+    playerDOM.innerHTML = player
+
+    const hideModal = document.querySelector('.modal-player')
+    hideModal.classList.add('hide-modal')
+
+    const showContent = document.querySelector('.container-game')
+    showContent.classList.add('show-content')
+
+    e.preventDefault()
+})
+//Ocultar modal
+
+//preguntas
+
+import { data } from "./preguntas.js";
+
+//---- Mostrar preguntas ----
+
+//Mostrar Texto pregunta 
+const showTextQuestion = (i) => {
+    const question = document.querySelector('.question')
+    const textQuestion = document.createElement("p")
+
+    textQuestion.innerHTML = `${data[i].titulo} ${data[i].pregunta}`
+    question.appendChild(textQuestion)
+}
+
+//Mostrar Opciones
+const showOptions = (i) => {
+    const options = document.querySelector('.options')
+    const fragment = document.createDocumentFragment()
+
+    let count = 1
+
+    data[i].opciones.forEach( e => {
+        const button = document.createElement('button')
+        button.innerHTML = e
+        button.classList.add('btn-choice')
+
+        count++
+        fragment.appendChild(button)
+    })
+    
+    options.appendChild(fragment)
+}
+
+
+//Borrar Preguntas Anteriores
+const deleteQuestion = () => {
+
+    //borrar la pregunta
+    const question = document.querySelector('.question')
+    question.removeChild(question.firstChild)
+
+    //borrar las opciones
+    const options = document.querySelector('.options')
+    options.innerHTML = ''
+}
+
+//comprobar respuestas correctas
+let points = 0
+
+const checkChoice = (i) => {
+    const choice = document.querySelectorAll('.btn-choice')
+    const pointsDOM = document.querySelector('.points-dom')
+
+    choice.forEach(el => {
+    el.addEventListener('click', e => {
+
+        //Desactivar buttons despues de responder
+        choice[0].disabled = true
+        choice[1].disabled = true
+        choice[2].disabled = true
+        choice[3].disabled = true
+
+        //mostrar puntos
+        if(e.target.innerHTML === data[i].correcta){
+            points++
+            pointsDOM.innerHTML = points
+        }else {
+            pointsDOM.innerHTML = points
+        }
+    })
+})
+}
+
+//Pasar a la siguiente pregunta
+const btnNext = document.getElementById('btn-next')
+let countP = 1
+
+btnNext.addEventListener('click', e => {
+    if(countP === data.length) {
+        //Aqui mostrar Resultado
+        alert(`---- NO HAY MAS PREGUNTAS ----`)
+    }else {
+        deleteQuestion()
+        showTextQuestion(countP)
+        showOptions(countP)
+        checkChoice(countP)
+    }
+    countP++
+})
+
+//salir
+const btnExit = document.querySelector('.btn-exit')
+btnExit.addEventListener('click', e => {
+    location.reload();
+})
+showTextQuestion(0)
+showOptions(0)
+checkChoice(0)
+
+
+
---
+++ js/preguntas.js
@@ -0,0 +1,57 @@
+export const data = [
+    {
+        titulo: "Pregunta 1",
+        pregunta: "¿Qué serie de dibujos animados tiene la serie de televisión y la comedia de situación más largas en términos de episodios y temporadas en los Estados Unidos??",
+        opciones: [
+            "Doctor Who",
+            "Dragon Ball",
+            "The Simpsons", 
+            "The Bugs Bunny and Tweety Show"
+        ],
+        correcta: "The Simpsons"
+    },
+    {
+        titulo: "Pregunta 2",
+        pregunta: "Cuando el Demonio de Tasmania hizo su debut, ¿quién protagonizó la caricatura?",
+        opciones: [
+            "Bugs Bunny",
+            "The Beatles",
+            "Mickey Mouse",
+            "Bart Simpsons"   
+        ],
+        correcta: "Bugs Bunny",
+    },
+    {
+        titulo: "Pregunta 3",
+        pregunta: "¿Quién es el antagonista en el “Rey León”?",
+        opciones: [
+            "Shenzi",
+            "Timón y Pumba",
+            "Rafiki",
+            "Scar"
+        ],
+        correcta: "Scar",
+    },
+    {
+        titulo: "Pregunta 4",
+        pregunta: "¿Qué serie de dibujos animados muestra a un luchador atrapado en el futuro que debe conquistar a un demonio para volver a casa?",
+        opciones: [
+            "Demon Slayer",
+            "Samurai Jack",
+            "Jujutsu Kaisen",
+            "Looney Tunes"
+        ],
+        correcta: "Samurai Jack",
+    },
+    {
+        titulo: "Pregunta 5",
+        pregunta: "¿Cómo se llama el perro de la serie “Tom y Jerry?",
+        opciones: [
+            "Goofy",
+            "Timón y Pumba",
+            "Akamaru",
+            "Spike"
+        ],
+        correcta: "Spike",
+    }
+]
